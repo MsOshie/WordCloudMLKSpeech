c043cdbb389559d991880ccaac7cb4e23085e1a8
MsOshie <stephyoge@mail.usf.edu>
2018-04-24 13:02:00 -0400

    Delete loadAsCorpus2.R

--- loadAsCorpus2.R
+++
@@ -1,8 +0,0 @@
-#' Load as corpus
-#'
-#' @details VectorSource() function creates a corpus of character vectors
-#' @param the text file
-#' @return nothing
-#' @export
-speech1 <- Corpus(VectorSource(speech))
-
